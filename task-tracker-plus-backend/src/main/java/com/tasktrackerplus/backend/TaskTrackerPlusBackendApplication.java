089935814b5834bd64f2fe70be62b8daa5e7fe8d
mkaj14 <kajb7547@stthomas.edu>
2026-02-23 18:53:12 -0600

    Add files via upload

---
+++ task-tracker-plus-backend/src/main/java/com/tasktrackerplus/backend/TaskTrackerPlusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tasktrackerplus.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerPlusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerPlusBackendApplication.class, args);
+	}
+
+}
---
+++ task-tracker-plus-backend/src/test/java/com/tasktrackerplus/backend/TaskTrackerPlusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasktrackerplus.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerPlusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
